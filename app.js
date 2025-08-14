8f5a76daba247494f7bc056d29164a411ea9b109
Santiago Monsalve <monsalvesantiago582@gmail.com>
2025-08-14 12:54:59 -0500

    Se implementa la funcionalidad de mostrar los nombres en pantalla

--- app.js
+++ app.js
@@ -9,10 +9,25 @@ function agregarAmigo() {
     } else {
         nombresAmigos.push(nombre);
     }
-    
+    console.log(nombresAmigos);
+
+    limpiarCaja();
+    actualizarLista();
 }
 
 // Funcion para limpiar la entrada
 function limpiarCaja(){
     document.getElementById('amigo').value = '';
+}
+
+// Funcion para mostrar en pantalla los nombres agregados
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos'); // Seleccionar el elemento <ul>
+    lista.innerHTML = ""; // lo vacia
+    
+    for(let i = 0; i <= (nombresAmigos.length-1); i++){
+        const li = document.createElement('li'); // creacion del elemento <li></li>
+        li.innerText = nombresAmigos[i]; // se captura el nombre de la lista de nombres
+        lista.appendChild(li); // se agrega a la lista
+    }
 }
