0b8aa56a0fced235cf1d8d0d84d4f3b19df466c8
Santiago Monsalve <monsalvesantiago582@gmail.com>
2025-08-14 12:14:02 -0500

    implementacion de la funcion agregar nombre y funcion limpiar caja

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+// Funcion que captura el valor y lo agrega al array 
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if (nombre === ""){
+        alert("Por favor ingrese un nombre");
+    } else {
+        nombresAmigos.push(nombre);
+    }
+    
+}
+
+// Funcion para limpiar la entrada
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
