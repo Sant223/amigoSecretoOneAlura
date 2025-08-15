10a2eb357e123bf7ca6f65fe07032f678a7f2cc2
Santiago Monsalve <monsalvesantiago582@gmail.com>
2025-08-15 16:50:36 -0500

    Se agrega la funcion para seleccionar el amigo secreto y la funcion para mostrar en pantalla el seleccionado

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos = [];
-
 // Funcion que captura el valor y lo agrega al array 
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
@@ -9,7 +8,6 @@ function agregarAmigo() {
     } else {
         nombresAmigos.push(nombre);
     }
-    console.log(nombresAmigos);
 
     limpiarCaja();
     actualizarLista();
@@ -30,4 +28,26 @@ function actualizarLista(){
         li.innerText = nombresAmigos[i]; // se captura el nombre de la lista de nombres
         lista.appendChild(li); // se agrega a la lista
     }
+}
+
+// Funcion para seleccionar el amigo secreto
+function sortearAmigo(){
+    if(nombresAmigos.length === 0){ // Si no hay nombres añadidos
+        alert("No hay amigos añadidos.");
+    }
+    
+    // Elije aleatoriamente un indice del array nombresAmigos
+    let indiceAleatorio = Math.floor(Math.random()*(nombresAmigos.length)); 
+
+    // Selecciona el nombre deacuerdo al indice
+    let nombreSeleccionado = nombresAmigos[indiceAleatorio];
+    
+    // Lo muestra en pantalla
+    mostrarSeleccionado(nombreSeleccionado);
+}
+
+// Funcion para mostrar en pantalla el nombre seleccionado
+function mostrarSeleccionado(nombreSeleccionado){
+    document.getElementById('resultado').innerHTML = `${nombreSeleccionado.charAt(0).toUpperCase() +
+        nombreSeleccionado.slice(1)} es el amigo secreto!`;
 }
